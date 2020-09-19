084e68c2fe8a5ae2ce91158d5154847ea45f5d57
Emily Martens <emartens@osihardware.com>
2020-09-19 14:02:51 -0700

    add more views to job_template bp, add task detail html page.

--- job_template/views.py
+++ job_template/views.py
@@ -11,8 +11,21 @@ def templates():
     'username': 'test@testing.com', 'email' : 'test@testing.com', 'timezone' : 'EST', 'view' : 'Month'
     }
     templates = [
-    {'id': 1, 'title':'Template 1'},
-    {'id': 2, 'title': 'Template 2'},
-    {'id' : 3, 'title':'Template 3'}
+    {'id': 0, 'title':'Template 1'},
+    {'id': 1, 'title': 'Template 2'},
+    {'id' : 2, 'title':'Template 3'}
     ]
     return render_template('job-template/templates.html', title='Templates', user=user, templates=templates)
+
+@bp.route('/template_detail/<id>')
+def template_detail(id):
+    user = {
+    'username': 'test@testing.com', 'email' : 'test@testing.com', 'timezone' : 'EST', 'view' : 'Month'
+    }
+    templates = [
+    {'id': 0, 'title':'Template 1', 'start_date' : '09.10.2020', 'interval': 3, 'interval_type' : 'Weeks', 'description' : 'Job Template description goes here.'},
+    {'id': 1, 'title': 'Template 2', 'start_date' : '09.15.2020', 'interval': 5, 'interval_type' : 'Days', 'description' : 'Job Template description goes here.'},
+    {'id' : 2, 'title':'Template 3', 'start_date' : '09.20.2020', 'interval' : 1, 'interval_type' : 'Months', 'description' : 'Job Template description goes here.'}
+    ]
+    template = templates[int(id)]
+    return render_template('job-template/template-detail.html', title='Template Details', user=user, template=template)
