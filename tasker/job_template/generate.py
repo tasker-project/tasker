22b73dc3e633b9e3803e88387e54b5516b9d38f4
Emily Martens <emartens@osihardware.com>
2020-09-25 23:22:17 -0700

    add Job Template task generation function, Job Template task deletion function, and Job Template update Tasks function.

---
+++ tasker/job_template/generate.py
@@ -0,0 +1,38 @@
+from tasker.models import db, User, JobTemplate, Task, TaskStatus
+import datetime
+
+def generate_tasks(template_id):
+    template = JobTemplate.query.get(template_id)
+    user = template.owner
+    rep = template.repetition
+    interval = template.interval
+    if interval == 2:
+        rep *= 7
+    if interval == 3:
+        rep *= 30
+    start = template.starting_date
+    timestamp = datetime.datetime.fromtimestamp(start)
+    end = timestamp.replace(timestamp.year + 1)
+    while timestamp <= end:
+        task = Task.create_task(template.name, template.description,TaskStatus.Pending, timestamp)
+        task.owner = user
+        task.job_template = template
+        db.session.add(task)
+        db.session.commit()
+        timestamp += datetime.timedelta(days=rep)
+    return 1
+
+def delete_tasks(template_id):
+    template = JobTemplate.query.get(template_id)
+    tasks = Task.query.filter_by(job_template=template)
+    for t in tasks:
+        db.session.delete(t)
+    db.session.commit()
+    return 1
+
+def update_job_template(template_id):
+    template = JobTemplate.query.get(template_id)
+    user_id = template.owner.email_address
+    delete_tasks(template_id)
+    generate_tasks(template_id)
+    return 1
