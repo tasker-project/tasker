6e875b7116ab8a195402197fa4e867196ef6cdfa
Emily Martens <emartens@osihardware.com>
2020-10-04 12:15:51 -0700

    finalize fields for form and template

--- tasker/task/forms.py
+++ tasker/task/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField, SubmitField, PasswordField, SelectField
+from wtforms import StringField,IntegerField, SubmitField, PasswordField, SelectField, validators
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 
@@ -10,31 +10,6 @@ class Task(FlaskForm):
     submit = SubmitField('Submit')
 
 class SnoozeTaskForm(FlaskForm):
-    hour = SelectField('Hour', validators=[DataRequired()], choices=[
-        ('0', '12 AM'),
-        ('1', '1 AM'),
-        ('2', '2 AM'),
-        ('3', '3 AM'),
-        ('4', '4 AM'),
-        ('5', '5 AM'),
-        ('6', '6 AM'),
-        ('7', '7 AM'),
-        ('8', '8 AM'),
-        ('9', '9 AM'),
-        ('10', '10 AM'),
-        ('11', '11 AM'),
-        ('12', '12 PM'),
-        ('13', '1 PM'),
-        ('14', '2 PM'),
-        ('15', '3 PM'),
-        ('16', '4 PM'),
-        ('17', '5 PM'),
-        ('18', '6 PM'),
-        ('19', '7 PM'),
-        ('20', '8 PM'),
-        ('21', '9 PM'),
-        ('22', '10 PM'),
-        ('23', '11 PM'),
-    ])
     due_date = DateField('Due Date', validators=[DataRequired()])
+    note = StringField('Note', [validators.Length(min=0, max=1000)])
     submit = SubmitField('Snooze')
