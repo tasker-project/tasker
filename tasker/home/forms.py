2e6790764d0bdfcf3e087f8fa3e985ab7429ce1c
Emily Martens <emartens@osihardware.com>
2020-10-04 19:49:38 -0700

    add completed button, routing. delete old home blueprint and jinja template.

--- tasker/home/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField, SubmitField, PasswordField, SelectField
-from wtforms.validators import DataRequired, EqualTo, email
-
-
-
-
