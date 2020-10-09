f42aa8260bbe1d1cbad22c5bf9559a0ff1c6e0a9
ADM Wayne Bryan <WayneB@WarwickFulfillment.local>
2020-10-09 08:05:13 -0400

    Fixed a couple of links reference task_details vs details.

--- tasker/task/views.py
+++ tasker/task/views.py
@@ -9,31 +9,6 @@ from tasker.task.forms import TaskForm, SnoozeTaskForm, DeleteTaskForm
 
 bp = Blueprint('task', __name__, static_folder='../static')
 
-@bp.route('/task_detail/<id>')
-@login_required
-def task_detail(id):
-    user = {
-    'username': 'test@testing.com', 'email' : 'test@testing.com', 'timezone' : 'EST', 'view' : 'Month'
-    }
-    templates = [
-    {'id': 0, 'title':'Template 1', 'start_date' : '09.10.2020', 'interval': 3, 'interval_type' : 'Weeks', 'description' : 'Job Template description goes here.'},
-    {'id': 1, 'title': 'Template 2', 'start_date' : '09.15.2020', 'interval': 5, 'interval_type' : 'Days', 'description' : 'Job Template description goes here.'},
-    {'id' : 2, 'title':'Template 3', 'start_date' : '09.20.2020', 'interval' : 1, 'interval_type' : 'Months', 'description' : 'Job Template description goes here.'}
-    ]
-    tasks = [
-    {'id' : 0, 'due_date' : '9.10.2020', 'template': templates[0]},
-    {'id' : 1, 'due_date' : '9.20.2020', 'template': templates[1]},
-    {'id' : 2, 'due_date' : '9.25.2020', 'template': templates[1]},
-    {'id' : 3, 'due_date' : '9.30.2020', 'template': templates[1]},
-    {'id' : 4, 'due_date' : '10.5.2020', 'template': templates[1]},
-    {'id' : 5, 'due_date' : '10.5.2020', 'template': templates[2]},
-    {'id' : 6, 'due_date' : '10.10.2020', 'template': templates[1]},
-    ]
-
-    task = tasks[int(id)]
-
-    return render_template("task/task-detail.html", title="Task Detail", user=user, task=task)
-
 @bp.route('/add_task', methods=['GET', 'POST'])
 @login_required
 def add_task():
